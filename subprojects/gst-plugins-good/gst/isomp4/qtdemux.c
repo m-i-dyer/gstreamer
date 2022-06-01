a4c723486feaf2117c45238ce3fff31e9aa461ec
Mike Dyer <mike.dyer@twitter>
2022-06-01 14:36:38 +0100

    QTDemux: Don't shift "position"
    9deb3c2 assumed position was offset by media_start time.  It is not.

--- subprojects/gst-plugins-good/gst/isomp4/qtdemux.c
+++ subprojects/gst-plugins-good/gst/isomp4/qtdemux.c
@@ -6479,11 +6479,6 @@ gst_qtdemux_loop_state_movie (GstQTDemux * qtdemux)
     if (!GST_CLOCK_TIME_IS_VALID (position))
       continue;
 
-    if (stream->segment_index != -1) {
-      QtDemuxSegment *segment = &stream->segments[stream->segment_index];
-      position += segment->media_start;
-    }
-
     /* position of -1 is EOS */
     if (position < min_time) {
       min_time = position;
